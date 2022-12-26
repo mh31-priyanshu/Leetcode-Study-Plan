f7ffad610287c49797c47c6f29672e5ec7f8a627
Priyanshu Lapkale <66089621+mh31-priyanshu@users.noreply.github.com>
2022-12-26 16:38:12 +0530

    Day 4 Problem 2 Data Structure level 1 (118)

---
+++ Data Structures/Level 1/Day  4/Pascal's Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>pascal(numRows);
+        pascal[0].push_back(1);
+        if(numRows==1){return pascal;}
+        pascal[1].push_back(1);
+        pascal[1].push_back(1);
+        if(numRows==2){return pascal;}
+        int prev;
+        for(int i=2;i<numRows;i++){
+            pascal[i].push_back(1);
+            for(int j=1;j<i;j++){
+                pascal[i].push_back(pascal[i-1][j-1]+pascal[i-1][j]);
+            }
+            pascal[i].push_back(1);
+        }
+        return pascal;
+    }
+};
