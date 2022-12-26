3ba964178fd3cc2bde707cb56cea6345060cb45d
Priyanshu Lapkale <66089621+mh31-priyanshu@users.noreply.github.com>
2022-12-26 16:44:04 +0530

    Day 6 Data Structure Level 1 (387)

---
+++ Data Structures/Level 1/Day 6/First Unique Character in a String.cpp
@@ -0,0 +1,26 @@
+struct store{
+    int index;
+    int size;
+};
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,store> m;
+        for(int i=s.length()-1;i>=0;i--){
+            auto it = m.find(s[i]);
+            if(it!=m.end()){
+                it->second.size+=1;
+            }else{
+                m[s[i]] = {i,1};
+            }
+        }
+        for(auto it = m.begin(); it != m.end(); it++){
+            if(it->second.size==1){
+                return it->second.index;
+            }
+        }
+        return -1;
+    }
+};
